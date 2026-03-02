607f8e984d64187c038706986d099ebdcf5c3074
foxmanoriginal <foxmanoriginal@gmail.com>
2026-03-02 09:23:21 +0300

    Delete saper_backend directory

--- saper_backend/saper_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from api.views_index import index
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-]
